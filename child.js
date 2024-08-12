ac152a222126790c9c66166ad7df195d7ef093de
ChervyachokMigo <github@svdgod.ru>
2024-08-12 22:34:00 +0500

    Update child.js

--- child.js
+++ child.js
@@ -4,8 +4,8 @@ const { parentPort, workerData } = require('node:worker_threads');
 // CHILD
 if ( !isMainThread ) {
 	const procedure =  require(workerData.procedure_path);
-	parentPort.on('message', (data_in) => {
-		const data_out = procedure(data_in, workerData.procedure_data);
+	parentPort.on('message', async (data_in) => {
+		const data_out = await procedure(data_in, workerData.procedure_data);
 		parentPort.postMessage(data_out);
 	});
 } else {
